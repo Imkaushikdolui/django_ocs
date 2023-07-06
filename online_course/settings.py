d7fab2d7d5278f6d72d9b57a1a4cb59b804b9d97
Kaushik dolui <98687700+Imkaushikdolui@users.noreply.github.com>
2023-07-06 10:54:32 +0530

    file_added

--- online_course/settings.py
+++ online_course/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#-$wj8uqiwn#y0kbjv9coju1aiw-)&cw14&(--g1*afgmfq-)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- online_course/urls.py
+++ online_course/urls.py
@@ -35,9 +35,8 @@ urlpatterns = [
     path('email2falogin/',views.LoginOtpVerify,name='LoginOtpverify'),
 
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
